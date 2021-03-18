28f04781304d4db4ffff0c0609db2f762e7f5950
fpetcast <f.pettrone@outlook.it>
2021-03-18 12:33:33 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
+var first;
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+class figure {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+}
+
+var apple = new figure("apple", "red");
+apple.taste = "good";
+var banana = new figure("banana", "yellow");
+function display(obj) {
+  first = document.getElementById("first");
+  var txt = " ";
+  for (var x in obj) {
+    txt += obj[x] + " ";
+  }
+  return (first.innerHTML = txt);
+}
+
+window.onload = display(banana);
